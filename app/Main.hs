e4ee173e16f12689ab5294309a875b160ca24fea
BLUE <danil228aeedj@mail.ru>
2024-10-12 11:33:36 +0300

    Fix tail recursion

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
-module Main where
+module Pain where
 
--- Recursion with pattern matching
-splitInt :: (Integral a) => a -> [a]
-splitInt 0 = []
-splitInt x = x `mod` 10 : splitInt (x `div` 10)
+-- Tail Recursion with pattern matching
+splitInt :: (Integral a) => a -> [a] -> [a]
+splitInt 0 xs = xs
+splitInt x xs= splitInt (x `div` 10)(x `mod` 10 : xs)
 
 -- Recursion with pattern matching
 sumAll :: (Integral a) => [a] -> a
@@ -12,32 +12,16 @@ sumAll (x : xs) = x ^ 5 + sumAll xs
 
 -- composition operator with conditional expression
 digitFifthPowers :: (Integral a) => a -> Bool
-digitFifthPowers a = if a == (sumAll $ splitInt a) then True else False
+digitFifthPowers a = if a == (sumAll $ splitInt a []) then True else False
 
 -- List comprehension with guards
 findSum :: (Integral a) => a
 findSum = sum [x | x <- [100 .. 354294], digitFifthPowers x]
 
--- Tail recursion with pattern matching
-splitIntHelper :: (Integral a) => a -> [a]
-splitIntHelper 0 = []
-splitIntHelper x = x `mod` 10 : splitInt (x `div` 10)
-
-splitInt2 :: (Integral a) => a -> [a]
-splitInt2 a = splitIntHelper a
-
--- Tail recursion with pattern matching
-sumAll2Helper :: (Integral a) => [a] -> a
-sumAll2Helper [] = 0
-sumAll2Helper (x : xs) = x ^ 5 + sumAll xs
-
-sumAll2 :: (Integral a) => [a] -> a
-sumAll2 xs = sumAll2Helper xs
-
 -- Function with guards
 digitFifthPowers2 :: (Integral a) => a -> Bool
 digitFifthPowers2 a
-  | a == (sumAll2 $ splitInt2 a) = True
+  | a == (sumAll $ splitInt a []) = True
   | otherwise = False
 
 -- List comprehension with guards
@@ -90,4 +74,3 @@ sum_custom ((Number a ys ) : xs) = a + sum_custom xs
 
 findSum4 :: (Integral a) => a
 findSum4 = sum_custom $ filter digitFifthPower2 (splitInt4 [100 .. 354294])
-
